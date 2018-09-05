7555a2cd2b525520cd3d069e82c06ea81a2522a0
rdamala <39735134+rdamala@users.noreply.github.com>
2018-09-05 06:50:16 -0400

    Add files via upload

---
+++ testCase1.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.WebDriver;
+
+public class testCase1 {
+	public static void main(String args[]) {
+
+	WebDriver driver = new FirefoxDriver();
+	
+	driver.get("https://google.com");
+	
+}
+
+}
